bb24916811e85fb2a6c76349ecf218f195b4346d
VladGradskiy <enbaev.vlad4@gmail.com>
2023-06-24 12:50:35 +0300

    Initial Commit

---
+++ YesOrNotApp/YesOrNotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YesOrNotAppApp.swift
+//  YesOrNotApp
+//
+//  Created by Влад Енбаев on 24.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct YesOrNotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
